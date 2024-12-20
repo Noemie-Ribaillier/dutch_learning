eb3374599f0c146a856245dbca31dca45a919172
Noemie Ribaillier <noemie.ribaillier@gmail.com>
2024-12-20 17:12:19 +0100

    update the app with the themes

--- app_dutch_verbs_translation.R
+++ app_dutch_verbs_translation.R
@@ -1,4 +1,12 @@
-## DUTCH TO ENGLISH TRANSLATION APPLICATION
+
+####################################################################################################################################
+#####                                                                                                                          #####
+#####                                                   APP DUTCH - ENGLISH                                                    #####
+#####                                                    RIBAILLIER NOEMIE                                                     #####
+#####                                              LAST MODIFICATION: 2024-12-20                                              `#####
+#####                                                                                                                          #####
+####################################################################################################################################
+
 
 # Load the needed libraries
 library(shiny)
@@ -7,13 +15,8 @@ library(DT)
 library(data.table)
 library(readODS)
 
-
-# Verbs with their translations
-verbs_excel <- data.frame(read_ods('C:/Users/Admin/Documents/Projets R/data/translation_dutch.ods',sheet ='verbs'))
-colnames(verbs_excel) <- c('dutch_verb','english_verb')
-verbs_excel[,c('attempts','successes')] <- 0
-verbs_excel$success_rate <- NA
-head(verbs_excel)
+# Set the Excel file to a library
+file <- "C:/Users/Admin/Documents/Projets R/dutch_learning/translation_dutch.ods"
 
 
 # Define UI
@@ -23,14 +26,15 @@ ui <- fluidPage(
   # left side of the page
   sidebarLayout(
     sidebarPanel(
-      # verb to translate, chosen randomly by the app (htmlOutput allows to change the font)
-      htmlOutput("verb_to_translate"),
+      selectizeInput(inputId = "theme", label = "Theme:",choices ='Verbs'),
+      # word to translate, chosen randomly by the app (htmlOutput allows to change the font)
+      htmlOutput("word_to_translate"),
       # text box where the user enters the translation
-      textInput("verb_translated", NULL), 
+      textInput("word_translated", NULL), 
       # button to check the translation
-      actionButton("check", "Check Translation",icon('circle-check'),style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-      # button to display a verb in the 1st use then to go to the next verb for the next uses
-      actionButton("next_verb", "Display a verb",icon('circle-arrow-right'),style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+      actionButton("check", "Check",icon('circle-check'),style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+      # button to display a word in the 1st use then to go to the next word for the next uses
+      actionButton("next_word", "Display a word",icon('circle-arrow-right'),style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
       # check if the translation is correct or not
       htmlOutput("translation_check"),
       # break line
@@ -39,18 +43,18 @@ ui <- fluidPage(
       htmlOutput("global_statistics"),
       # break line
       br(),
-      # download button, to download the output table with statistics per verb
+      # download button, to download the output table with statistics per word
       downloadButton('download',label = 'Download the statistics',icon('download'),
                      style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
     ),
     # main side of the page
     mainPanel(
       # title of the main page
-      h3('All verbs - statistics'),
+      h3('All words - statistics'),
       # options to remove the search zone
       tags$head(tags$style(type="text/css", ".dataTables_filter {display: none;    }")),
-      # output table, summing up for each verb the translation, number of attempts, successes and success rate
-      DTOutput("table_verbs_statistics")
+      # output table, summing up for each word the translation, number of attempts, successes and success rate
+      DTOutput("table_words_statistics")
     )
   )
 )
@@ -59,40 +63,60 @@ ui <- fluidPage(
 # Define server 
 server <- function(input, output, session) {
   
-  # Reactive value to store verbs data
-  verbs_data <- reactiveVal(verbs_excel)
+  # Read the sheet names from the Excel file and update the selectInput
+  themes <- sort(list_ods_sheets(file))
+  updateSelectInput(session, "theme", choices = themes)
   
-  # Reactive value to store the verb to translate
-  verb_to_translate <- reactiveVal(NULL)
+  words_data <- reactive({
+
+    data <- read_ods(file,sheet  = input$theme)  # Read the data from the selected sheet
+    colnames(data) <- c('dutch_word','english_word')
+    data[,c('attempts','successes')] <- 0
+    data$success_rate <- NA
+    return(data)
+  })
+    
+  # Reactive value to store words data
+  words_data <- reactiveVal(NULL)
+
+  # Reactive value to store the word to translate
+  word_to_translate <- reactiveVal(NULL)
   
-  # When clicking on the button "next", select a random verb
-  observeEvent(input$next_verb, {
-    # Get a random index, between 1 and the number of verbs
-    random_index <- sample(nrow(verbs_data()), 1)
-    # Pick the verb, based on the index
-    verb_to_translate(verbs_data()$english_verb[random_index])
-    # Display the verb to translate in the output
-    output$verb_to_translate <- renderText(paste("Translate:<b>", verb_to_translate(),'</b>'))
+  observe({
+
+    data <- read_ods('C:/Users/Admin/Documents/Projets R/dutch_learning/translation_dutch.ods',sheet  = input$theme)  # Read the data from the selected sheet
+    colnames(data) <- c('dutch_word','english_word')
+    data[,c('attempts','successes')] <- 0
+    data$success_rate <- NA
+    words_data(data)
+  })
+  
+  
+  # When clicking on the button "next", select a random word
+  observeEvent(input$next_word, {
+    # Get a random index, between 1 and the number of words
+    random_index <- sample(x = nrow(words_data()), size = 1,prob = ifelse(is.na(words_data()$success_rate),100,100-words_data()$success_rate))
+    # Pick the word, based on the index
+    word_to_translate(words_data()$english_word[random_index])
+    # Display the word to translate in the output
+    output$word_to_translate <- renderText(ifelse(input$theme=='verbs_present',paste("Conjugate:<b>", word_to_translate(),'</b>'),paste("Translate:<b>", word_to_translate(),'</b>')))
     # Re-new the text box (to have it back to blank)
-    updateTextInput(session, "verb_translated", value = "")
+    updateTextInput(session, "word_translated", value = "")
   })
   
   # When clicking on the button "check", check for the translation and update the statistics
   observeEvent(input$check, {
-    ################################################# TO CHECK IF NEEDED
-    # req(verb_to_translate())
-    # 
-    # user_translation <- input$verb_translated
-    current_data <- verbs_data()
     
-    # Update attempts for the selected verb (verb to translate) [+1]
-    current_data[current_data$english_verb == verb_to_translate(), "attempts"] <- current_data[current_data$english_verb == verb_to_translate(), "attempts"] + 1
+    current_data <- words_data()
+    
+    # Update attempts for the selected word (word to translate) [+1]
+    current_data[current_data$english_word == word_to_translate(), "attempts"] <- current_data[current_data$english_word == word_to_translate(), "attempts"] + 1
     
     # Check translation
-    correct_translation <- current_data[current_data$english_verb == verb_to_translate(), "dutch_verb"]
-    if (input$verb_translated == correct_translation) {
-      # Update the successes for the selected verb (verb to translate), if correct [+1]
-      current_data[current_data$english_verb == verb_to_translate(), "successes"] <- current_data[current_data$english_verb == verb_to_translate(), "successes"] + 1
+    correct_translation <- current_data[current_data$english_word == word_to_translate(), "dutch_word"]
+    if (input$word_translated == correct_translation) {
+      # Update the successes for the selected word (word to translate), if correct [+1]
+      current_data[current_data$english_word == word_to_translate(), "successes"] <- current_data[current_data$english_word == word_to_translate(), "successes"] + 1
       # Update the output with correct and green if correct
       output$translation_check <- renderText({ paste("<font color=\"#008000\"><b>", 'Correct!', "</b></font>") })
     } else {
@@ -101,36 +125,36 @@ server <- function(input, output, session) {
     }
     
     # Update the success rate
-    current_data[current_data$english_verb == verb_to_translate(), "success_rate"] <- round(current_data[current_data$english_verb == verb_to_translate(), "successes"]*100/current_data[current_data$english_verb == verb_to_translate(), "attempts"],0)
+    current_data[current_data$english_word == word_to_translate(), "success_rate"] <- round(current_data[current_data$english_word == word_to_translate(), "successes"]*100/current_data[current_data$english_word == word_to_translate(), "attempts"],0)
     
-    # Update the global statistics by doing the sum over the statistics per verb
+    # Update the global statistics by doing the sum over the statistics per word
     output$global_statistics <- renderText({ paste('Successes: ','<b>',sum(current_data$successes),
-                                         '</b>',' | Attempts: ','<b>',sum(current_data$attempts),
-                                         '</b>',' | Success Rate: ','<b>',round(sum(current_data$successes)*100/sum(current_data$attempts)),'%</b>') })
+                                                   '</b>',' | Attempts: ','<b>',sum(current_data$attempts),
+                                                   '</b>',' | Success Rate: ','<b>',round(sum(current_data$successes)*100/sum(current_data$attempts)),'%</b>') })
     
-    # Update initial verbs data
-    verbs_data(current_data)
+    # Update initial words data
+    words_data(current_data)
   })
   
   # Set up the output dataframe (in the main page)
-  output$table_verbs_statistics <- renderDataTable({
+  output$table_words_statistics <- renderDataTable({
     # We change the column names and remove the rownames
-    datatable(verbs_data(), colnames = c('Dutch','English','Attempts','Successes','Success Rate'),rownames = NULL) %>% 
-      # Success rate is blank if the verb hasn't been selected yet, red if the success rate is between 0 and 60%, green above
+    datatable(words_data(), colnames = c('Dutch','English','Attempts','Successes','Success Rate'),rownames = NULL) %>% 
+      # Success rate is blank if the word hasn't been selected yet, red if the success rate is between 0 and 60%, green above
       formatStyle('success_rate',backgroundColor=styleInterval(cuts = c(60),values = c('red','green')))
   })
   
   # Set up the download button
   output$download <- downloadHandler(
     # Filename is the date pasted with some name
-    filename = function() {paste(gsub(pattern = '-',replacement = '_',x = Sys.Date()),'translation_verbs_statistics.csv', sep='_')},
-    # Content is the verbs table (with statistics per verb)
-    content = function(con) {write.csv(verbs_data(), con,row.names=F)}
-    )
+    filename = function() {paste(gsub(pattern = '-',replacement = '_',x = Sys.Date()),'translation_words_statistics.csv', sep='_')},
+    # Content is the words table (with statistics per word)
+    content = function(con) {write.csv(words_data(), con,row.names=F)}
+  )
   
   # Update the button: "display" for the 1st use then for the next uses "next"
-  observeEvent(input$next_verb, {
-    updateActionButton(session, "next_verb", label = 'Next verb')
+  observeEvent(input$next_word, {
+    updateActionButton(session, "next_word", label = 'Next word')
   })
 }
 
@@ -139,7 +163,3 @@ shinyApp(ui = ui, server = server)
 
 
 
-
-# IMPROVEMENTS
-# put a higher proba to verbs not succeeded
-# themes
